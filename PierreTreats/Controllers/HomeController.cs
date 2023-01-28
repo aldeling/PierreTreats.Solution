ba8c369125b012fb15df74d7b72958cf2f02dc40
Athea DeLing <almdeling@gmail.com>
2023-01-27 23:46:48 -0500

    add home controller

---
+++ PierreTreats/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using PierreTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierreTreats.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierreTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public HomeController(UserManager<ApplicationUser> userManager, PierreTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("flavors", flavors);
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      if (currentUser != null)
+      {
+        Treat[] treats = _db.Treats
+          .Where(entry => entry.User.Id == currentUser.Id)
+          .ToArray();
+        model.Add("treats", treats);
+      }
+      return View(model);
+    }
+  }
+}
